a1e2158179122cf2ca5ccc8e4fd971affcc8a5d7
Matheus Jimenez Falzetta <58031797+matheusjimenez@users.noreply.github.com>
2020-09-18 18:05:29 -0300

    First release of Face detection 1.0
    Using microsoft Azure to detect faces.

---
+++ FaceTutorial/FaceTutorial/MainWindow.xaml.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System.IO;
+
+namespace FaceTutorial
+{
+
+    public partial class MainWindow : Window
+    {
+        // aqui voce deve adicionar uma variável de ambiente no seu console
+        //aperte windows + R
+        //escreva cmd e dê enter
+        //no console escreva > setx FACE_SUBSCRIPTION_KEY 'insira aqui sem aspas a sua key da azure'
+        private static string subscriptionKey = Environment.GetEnvironmentVariable("FACE_SUBSCRIPTION_KEY");
+        // setx FACE_ENDPOINT2 'insira aqui o endpoint da azure'
+        private static string faceEndpoint = Environment.GetEnvironmentVariable("FACE_ENDPOINT2");
+
+        private readonly IFaceClient faceClient = new FaceClient(
+            new ApiKeyServiceClientCredentials(subscriptionKey),
+            new System.Net.Http.DelegatingHandler[] { });
+
+        // uma lista para armazenar as faces detectadas
+        private IList<DetectedFace> faceList;
+        // lista de descrição das faces
+        private string[] faceDescriptions;
+        // ajustando o tamanho da imagem exibida
+        private double resizeFactor;
+
+        private const string defaultStatusBarText =
+            "Arraste o mouse até uma face para ver a descrição.";
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (Uri.IsWellFormedUriString(faceEndpoint, UriKind.Absolute))
+            {
+                faceClient.Endpoint = faceEndpoint;
+            }
+            else
+            {
+                MessageBox.Show(faceEndpoint,
+                    "Url do endpoint inválida!", MessageBoxButton.OK, MessageBoxImage.Error);
+                Environment.Exit(0);
+            }
+        }
+
+
+
+        // Displays the image and calls UploadAndDetectFaces.
+        private async void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the image file to scan from the user.
+            var openDlg = new Microsoft.Win32.OpenFileDialog();
+
+            openDlg.Filter = "JPEG Image(*.jpg)|*.jpg";
+            bool? result = openDlg.ShowDialog(this);
+
+            // Return if canceled.
+            if (!(bool)result)
+            {
+                return;
+            }
+
+            // Display the image file.
+            string filePath = openDlg.FileName;
+
+            Uri fileUri = new Uri(filePath);
+            BitmapImage bitmapSource = new BitmapImage();
+
+            bitmapSource.BeginInit();
+            bitmapSource.CacheOption = BitmapCacheOption.None;
+            bitmapSource.UriSource = fileUri;
+            bitmapSource.EndInit();
+
+            FacePhoto.Source = bitmapSource;
+
+            // Detect any faces in the image.
+            Title = "Detecting...";
+            faceList = await UploadAndDetectFaces(filePath);
+            Title = String.Format(
+                "Detection Finished. {0} face(s) detected", faceList.Count);
+
+            if (faceList.Count > 0)
+            {
+                // Prepare to draw rectangles around the faces.
+                DrawingVisual visual = new DrawingVisual();
+                DrawingContext drawingContext = visual.RenderOpen();
+                drawingContext.DrawImage(bitmapSource,
+                    new Rect(0, 0, bitmapSource.Width, bitmapSource.Height));
+                double dpi = bitmapSource.DpiX;
+                // Some images don't contain dpi info.
+                resizeFactor = (dpi == 0) ? 1 : 96 / dpi;
+                faceDescriptions = new String[faceList.Count];
+
+                for (int i = 0; i < faceList.Count; ++i)
+                {
+                    DetectedFace face = faceList[i];
+
+                    // Draw a rectangle on the face.
+                    drawingContext.DrawRectangle(
+                        Brushes.Transparent,
+                        new Pen(Brushes.Red, 2),
+                        new Rect(
+                            face.FaceRectangle.Left * resizeFactor,
+                            face.FaceRectangle.Top * resizeFactor,
+                            face.FaceRectangle.Width * resizeFactor,
+                            face.FaceRectangle.Height * resizeFactor
+                            )
+                    );
+
+                    // Store the face description.
+                    faceDescriptions[i] = FaceDescription(face);
+                }
+
+                drawingContext.Close();
+
+                // Display the image with the rectangle around the face.
+                RenderTargetBitmap faceWithRectBitmap = new RenderTargetBitmap(
+                    (int)(bitmapSource.PixelWidth * resizeFactor),
+                    (int)(bitmapSource.PixelHeight * resizeFactor),
+                    96,
+                    96,
+                    PixelFormats.Pbgra32);
+
+                faceWithRectBitmap.Render(visual);
+                FacePhoto.Source = faceWithRectBitmap;
+
+                // Set the status bar text.
+                faceDescriptionStatusBar.Text = defaultStatusBarText;
+            }
+        }
+
+
+
+        // Displays the face description when the mouse is over a face rectangle.
+        private void FacePhoto_MouseMove(object sender, MouseEventArgs e)
+        {
+            // If the REST call has not completed, return.
+            if (faceList == null)
+                return;
+
+            // Find the mouse position relative to the image.
+            Point mouseXY = e.GetPosition(FacePhoto);
+
+            ImageSource imageSource = FacePhoto.Source;
+            BitmapSource bitmapSource = (BitmapSource)imageSource;
+
+            // Scale adjustment between the actual size and displayed size.
+            var scale = FacePhoto.ActualWidth / (bitmapSource.PixelWidth / resizeFactor);
+
+            // Check if this mouse position is over a face rectangle.
+            bool mouseOverFace = false;
+
+            for (int i = 0; i < faceList.Count; ++i)
+            {
+                FaceRectangle fr = faceList[i].FaceRectangle;
+                double left = fr.Left * scale;
+                double top = fr.Top * scale;
+                double width = fr.Width * scale;
+                double height = fr.Height * scale;
+
+                // Display the face description if the mouse is over this face rectangle.
+                if (mouseXY.X >= left && mouseXY.X <= left + width &&
+                    mouseXY.Y >= top && mouseXY.Y <= top + height)
+                {
+                    faceDescriptionStatusBar.Text = faceDescriptions[i];
+                    mouseOverFace = true;
+                    break;
+                }
+            }
+
+            // String to display when the mouse is not over a face rectangle.
+            if (!mouseOverFace) faceDescriptionStatusBar.Text = defaultStatusBarText;
+        }
+
+        private async Task<IList<DetectedFace>> UploadAndDetectFaces(string imageFilePath)
+        {
+            // The list of Face attributes to return.
+            IList<FaceAttributeType?> faceAttributes =
+                new FaceAttributeType?[]
+                {
+            FaceAttributeType.Gender, FaceAttributeType.Age,
+            FaceAttributeType.Smile, FaceAttributeType.Emotion,
+            FaceAttributeType.Glasses, FaceAttributeType.Hair
+                };
+
+            // Call the Face API.
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(imageFilePath))
+                {
+                    // The second argument specifies to return the faceId, while
+                    // the third argument specifies not to return face landmarks.
+                    IList<DetectedFace> faceList =
+                        await faceClient.Face.DetectWithStreamAsync(
+                            imageFileStream, true, false, faceAttributes);
+                    return faceList;
+                }
+            }
+            // Catch and display Face API errors.
+            catch (APIErrorException f)
+            {
+                MessageBox.Show(f.Message);
+                return new List<DetectedFace>();
+            }
+            // Catch and display all other errors.
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error");
+                return new List<DetectedFace>();
+            }
+        }
+
+        // Creates a string out of the attributes describing the face.
+        private string FaceDescription(DetectedFace face)
+        {
+            StringBuilder sb = new StringBuilder();
+            string genero = "Não identificado";
+            sb.Append("Face: ");
+
+            if (face.FaceAttributes.Gender.Equals("Male"))
+                genero = "Semelhante ao genero masculino";
+            else
+                genero = "Semelhante o genero feminino";
+            // Adiciona genero, idade e sorriso
+            sb.Append(face.FaceAttributes.Gender);
+            sb.Append(", ");
+            sb.Append(face.FaceAttributes.Age);
+            sb.Append(", ");
+            sb.Append(String.Format("Sorriso {0:F1}%, ", face.FaceAttributes.Smile * 100));
+
+            // adiciona as emoções esboçadas acima de 10%
+            sb.Append("Emoções: ");
+            Emotion emotionScores = face.FaceAttributes.Emotion;
+            if (emotionScores.Anger >= 0.1f) sb.Append(
+                String.Format("Raiva {0:F1}%, ", emotionScores.Anger * 100));
+            if (emotionScores.Contempt >= 0.1f) sb.Append(
+                String.Format("Desprezo {0:F1}%, ", emotionScores.Contempt * 100));
+            if (emotionScores.Disgust >= 0.1f) sb.Append(
+                String.Format("Desgosto {0:F1}%, ", emotionScores.Disgust * 100));
+            if (emotionScores.Fear >= 0.1f) sb.Append(
+                String.Format("Medo {0:F1}%, ", emotionScores.Fear * 100));
+            if (emotionScores.Happiness >= 0.1f) sb.Append(
+                String.Format("Felicidade {0:F1}%, ", emotionScores.Happiness * 100));
+            if (emotionScores.Neutral >= 0.1f) sb.Append(
+                String.Format("Afeição neutra {0:F1}%, ", emotionScores.Neutral * 100));
+            if (emotionScores.Sadness >= 0.1f) sb.Append(
+                String.Format("Tristeza {0:F1}%, ", emotionScores.Sadness * 100));
+            if (emotionScores.Surprise >= 0.1f) sb.Append(
+                String.Format("Surpresa {0:F1}%, ", emotionScores.Surprise * 100));
+
+            // Adiciona os oculos usados
+            sb.Append(face.FaceAttributes.Glasses);
+            sb.Append(", ");
+
+            // adiciona o cabelo
+            sb.Append("Cabelo: ");
+
+            // Mostra o nivel de calvice se for maior que 1%
+            if (face.FaceAttributes.Hair.Bald >= 0.01f)
+                sb.Append(String.Format("Calvice {0:F1}% ", face.FaceAttributes.Hair.Bald * 100));
+
+            // Mostra todas as cores do cabelo que compoe mais de 10% do total
+            IList<HairColor> hairColors = face.FaceAttributes.Hair.HairColor;
+            foreach (HairColor hairColor in hairColors)
+            {
+                if (hairColor.Confidence >= 0.1f)
+                {
+                    sb.Append(hairColor.Color.ToString());
+                    sb.Append(String.Format(" {0:F1}% ", hairColor.Confidence * 100));
+                }
+            }
+
+            // retorna a descrição construida pelo stringbuilder
+            return sb.ToString();
+        }
+    }
+}
